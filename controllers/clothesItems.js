b0b2eafe40e61edaa0e17fa48a58d24d8ea04712
abinocoe <abigailcoe@gmail.com>
2016-09-30 14:46:18 +0100

    things we don't want

--- controllers/clothesItems.js
+++ controllers/clothesItems.js
@@ -11,16 +11,22 @@ const ClothesItem = require("../models/clothesItem");
 function clothesItemsIndex(req, res) {
   ClothesItem.find((err, clothesItems) => {
     if (err) return res.status(500).json({ message: "Something went wrong" });
-    return res.status(200).json(clothesItems);
+    return res.status(200).json({clothesItems});
   });
 }
 
 function clothesItemsCreate(req, res) {
-  
+  const clothesItem = new ClothesItem(req.body.clothesItem);
+  clothesItem.save((err, clothesItem) => {
+    if (err) return res.status(500).json({ message: "Something went wrong" });
+    return res.status(201).json({clothesItem});
+  });
 }
 
 function clothesItemsShow(req, res) {
-
+  ClothesItem.findById(req.params.id, (err, clothesItem) => {
+    if (err) return res.status(500).json({ message: "Something went wrong" });
+  });
 }
 
 function clothesItemsUpdate(req, res) {
