163ee08af6e0f428e1f9c6aac5ac20ee7a22f87b
DanTheGeekMan <danthegeekman@googlemail.com>
2016-09-30 12:09:12 +0100

    Put functions in homeCtrlr and updated form names.

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -1,9 +1,32 @@
 angular
-  .module("swishListApp")
-  .controller("HomeCtrl", HomeCtrl);
+.module("swishListApp")
+.controller("HomeCtrl", HomeCtrl);
 
-HomeCtrl.$inject = [];
-function HomeCtrl(){
+HomeCtrl.$inject = ["User", "CurrentUserService"];
+function HomeCtrl(User, CurrentUserService){
   const vm = this;
 
+  vm.register = () => {
+    User
+      .register( { user : vm.userRegister })
+      .$promise
+      .then(data => {
+        const user = data.user || null;
+        if (user){
+        CurrentUserService.saveUser(user);
+      }
+      });
+  };
+
+  vm.login = () => {
+    User
+    .login(vm.userLogin)
+    .$promise
+    .then(data => {
+      const user = data.user || null;
+      if (user){
+        CurrentUserService.saveUser(user);
+      }
+    });
+  };
 }
