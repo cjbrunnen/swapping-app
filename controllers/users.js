13a799a6c29ad722c24478c4c169f64c609065c6
delamorinierejh <delamorinierejh@gmail.com>
2016-10-01 13:21:56 +0100

    wrote users tests

--- controllers/users.js
+++ controllers/users.js
@@ -1,18 +1,9 @@
 module.exports = {
-  index:  usersIndex,
   show:   usersShow,
-  update: usersUpdate,
-  delete: usersDelete
+  update: usersUpdate
 };
 
 const User = require('../models/user');
-
-function usersIndex(req, res) {
-  User.find((err, users) => {
-    if (err) return res.status(500).json({ message: "Something went wrong." });
-    return res.status(200).json({ users });
-  });
-}
 
 function usersShow(req, res) {
   User.findById(req.params.id, (err, user) => {
@@ -27,13 +18,5 @@ function usersUpdate(req, res) {
     if (err) return res.status(500).json({ message: "Something went wrong." });
     if (!user) return res.status(404).json({ message: "User not found." });
     return res.status(200).json({ user });
-  });
-}
-
-function usersDelete(req, res) {
-  User.findByIdAndRemove(req.params.id, (err, user) => {
-    if (err) return res.status(500).json({ message: "Something went wrong." });
-    if (!user) return res.status(404).json({ message: "User not found." });
-    return res.status(204).send();
   });
 }
