b3b6ce4fb9a2d89160de919feb71eadbe6ef1610
delamorinierejh <delamorinierejh@gmail.com>
2016-10-01 14:10:05 +0100

    tidied up the navbar and organised the views and states available from topnav

--- src/js/configs/router.js
+++ src/js/configs/router.js
@@ -16,7 +16,33 @@ function Router($stateProvider, $urlRouterProvider, $locationProvider){
     url: '/clothesItems',
     templateUrl: '/js/views/clothesItems/index.html',
     // controller: 'ClothesItemsIndexCtrl as clothesItems'
-  });
+  })
+  .state('clothesItemsNew', {
+    url: '/clothesItems/new',
+    templateUrl: '/js/views/clothesItems/new.html',
+    // controller: 'ClothesItemsNewCtrl as clothesItems'
+  })
+  .state('usersWardrobeShow', {
+    url: '/user/wardrobe',
+    templateUrl: '/js/views/users/wardrobeShow.html',
+    // controller: 'WardrobeShowCtrl as wardrobe'
+  })
+  .state('usersIncomingShow', {
+    url: '/user/inbox',
+    templateUrl: '/js/views/users/incomingShow.html',
+    // controller: 'IncomingShowCtrl as incoming'
+  })
+  .state('usersOutgoingShow', {
+    url: '/user/outbox',
+    templateUrl: '/js/views/users/outgoingShow.html',
+    // controller: 'OutgoingShowCtrl as outgoing'
+  })
+  .state('usersArchiveShow', {
+    url: '/user/archive',
+    templateUrl: '/js/views/users/completedShow.html',
+    // controller: 'ArchiveShowCtrl as archive'
+  })
+  ;
 
   $urlRouterProvider.otherwise("/");
 
