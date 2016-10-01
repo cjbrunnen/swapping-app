ee9665692fb8a5478261b5035b8be51723504c52
delamorinierejh <delamorinierejh@gmail.com>
2016-10-01 12:30:36 +0100

    clothes exit tests done

--- test/api/itemsTests.js
+++ test/api/itemsTests.js
@@ -6,11 +6,6 @@ const User = require("../../models/user");
 describe("Clothes Items Controller Test", function() {
 
   beforeEach(done => {
-    ClothesItem.collection.drop();
-    done();
-  });
-
-  beforeEach(done => {
     const user = new User({
       username: "test",
       email: "test@test.com",
@@ -154,6 +149,69 @@ describe("Clothes Items Controller Test", function() {
       .expect(401, done);
     });
 
+  });
+
+  describe( "PUT /api/clothesItems:/id", function(done) {
+
+    beforeEach(done => {
+      const item = new ClothesItem({
+        title:        "Diesel Jeans",
+        description:  "These stonewashed jeans are tight fitting and lovely",
+        category:     "Jeans",
+        sex:          "Male",
+        image:        "http://i.ebayimg.com/images/g/RfsAAOSwq7JT9Ygz/s-l300.jpg",
+        available:    true
+      });
+      item.save((err, item) => {
+        ID2HERE = item._id;
+        done();
+      });
+    });
+
+    it("should return a 200 when an authorised user updates an exisitng item", done =>{
+      api.put(`/api/clothesItems/${ID2HERE}`)
+      .set('Accept', 'application/json')
+      .set("Authorization", `Bearer ${TOKEN}`)
+      .send({
+        title:        "Topman Jeans",
+        description:  "These stonewashed jeans are tight fitting and lovely",
+        category:     "Jeans",
+        sex:          "Male",
+        image:        "http://i.ebayimg.com/images/g/RfsAAOSwq7JT9Ygz/s-l300.jpg",
+        available:    true
+      })
+      .expect(200, done);
+    });
+
+    it("should return a 401 when an unauthorised user updates an exisitng item", done =>{
+      api.put(`/api/clothesItems/${ID2HERE}`)
+      .set('Accept', 'application/json')
+      .send({
+        title:        "Topman Jeans",
+        description:  "These stonewashed jeans are tight fitting and lovely",
+        category:     "Jeans",
+        sex:          "Male",
+        image:        "http://i.ebayimg.com/images/g/RfsAAOSwq7JT9Ygz/s-l300.jpg",
+        available:    true
+      })
+      .expect(401, done);
+    });
+
+
+    it("should return a 404 when an authorised user tries to update a non-exsitent item", done =>{
+      api.put(`/api/clothesItems/${IDHERE}`)
+      .set('Accept', 'application/json')
+      .set("Authorization", `Bearer ${TOKEN}`)
+      .send({
+      })
+      .expect(404, done);
+    });
+
+  });
+
+  afterEach(done => {
+    ClothesItem.collection.drop();
+    done();
   });
 
 });
