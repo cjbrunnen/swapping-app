9d29cabb009d2fc7a7ea900dd9fbf437e932f929
abinocoe <abigailcoe@gmail.com>
2016-10-01 13:32:30 +0100

    first two transactions controllers

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -9,11 +9,19 @@ module.exports = {
 const Transaction = require('../models/transaction');
 
 function transactionsCreate(req, res){
-  const transaction = new Transaction(req.body);
+  const transaction = new Transaction(req.body.transaction);
+  transaction.save((err, transaction) => {
+    if (err) return res.status(500).json({ message: "Something went wrong" });
+    return res.status(201).json({ transaction });
+  });
 }
 
-function transactionsSwishback(){
-
+function transactionsSwishback(req, res){
+  Transaction.findByIdAndUpdate(req.params.id, req.body.transaction, { new: true }, (err, transaction) => {
+    if (err) return res.status(500).json({ message: "Something went wrong" });
+    if (!transaction) return res.status(404).json({ message: "Swish not found" });
+    return res.status(200).json({ transaction });
+  });
 }
 
 function transactionsApprove(){
