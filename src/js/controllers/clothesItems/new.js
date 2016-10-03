a35827b04531231a7234f7fd76815f52561d5844
DanTheGeekMan <danthegeekman@googlemail.com>
2016-10-03 13:57:35 +0100

    Edit form not funcitonal.

--- src/js/controllers/clothesItems/new.js
+++ src/js/controllers/clothesItems/new.js
@@ -5,10 +5,6 @@ angular
 ClothesItemsNewCtrl.$inject = ["ClothesItem", "$state", 'CurrentUserService'];
 function ClothesItemsNewCtrl(ClothesItem, $state, CurrentUserService){
   const vm = this;
-  vm.item = {
-    owner: CurrentUserService.getUser().id,
-    available: true
-  };
   vm.submit = () => {
     ClothesItem
       .save({ clothesItem: vm.item })
