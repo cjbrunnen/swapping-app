2e8207e2b657302360f2648816418ece10c4e765
delamorinierejh <delamorinierejh@gmail.com>
2016-10-03 17:12:57 +0100

    HTML for index of swishback

--- src/js/configs/router.js
+++ src/js/configs/router.js
@@ -53,12 +53,12 @@ function Router($stateProvider, $urlRouterProvider, $locationProvider){
     controller: 'ArchiveShowCtrl as archive'
   })
   .state('swishbackIndex', {
-    url: '/responder/wardrobe',
+    url: '/:ownerid/wardrobe',
     templateUrl: '/js/views/swishbacks/index.html',
     controller: 'SwishbackIndexCtrl as swishback'
   })
   .state('swishbackShow', {
-    url: '/responder/wardrobe/:id',
+    url: '/:ownerid/wardrobe/:itemid',
     templateUrl: '/js/views/swishbacks/show.html',
     controller: 'SwishbackShowCtrl as swishback'
   })
