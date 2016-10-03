de3e7f5e9fec44ea15dc07f814c50146c96ef414
cjbrunnen <cazgotsoul@gmail.com>
2016-10-03 12:48:58 +0100

    Caz morning work

---
+++ src/js/controllers/users/incomingShow.js
@@ -0,0 +1,10 @@
+angular
+  .module("swishListApp")
+  .controller("IncomingShowCtrl", IncomingShowCtrl);
+
+  IncomingShowCtrl.$inject = [];
+  function IncomingShowCtrl(){
+    console.log("hello, working");
+  }
+
+showInitiatorItem, linkToInitiatorWardrobe, showResponseItem, statusChangingButtons
