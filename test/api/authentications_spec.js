772b032e715a6afe0845a722e73eed485a105ddb
abinocoe <abigailcoe@gmail.com>
2016-10-03 12:44:20 +0100

    sorted out current user issues

--- test/api/authentications_spec.js
+++ test/api/authentications_spec.js
@@ -2,14 +2,12 @@ require("../spec_helper");
 
 const User = require('../../models/user');
 
-User.collection.drop();
-
 describe("Auth tests", function() {
 
   // this.timeout(5000);
 
   beforeEach(done => {
-    User.collection.drop();
+    User.collection.remove();
     done();
   });
 
