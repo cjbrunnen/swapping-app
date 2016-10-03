b548e9ef92cdfd577a98ef9bce3544501ff8b3d2
cjbrunnen <cazgotsoul@gmail.com>
2016-10-03 15:08:51 +0100

    added transaction factory

--- src/js/controllers/users/incomingShow.js
+++ src/js/controllers/users/incomingShow.js
@@ -2,16 +2,17 @@ angular
   .module("swishListApp")
   .controller("IncomingShowCtrl", IncomingShowCtrl);
 
-  IncomingShowCtrl.$inject = ["ClothesItem", "CurrentUserService"];
-  function IncomingShowCtrl(ClothesItem, CurrentUserService){
+  IncomingShowCtrl.$inject = ["Transaction", "CurrentUserService"];
+  function IncomingShowCtrl(Transaction, CurrentUserService){
     const vm = this;
-    ClothesItem
+    Transaction
     .query()
     .$promise
     .then(data => {
-      vm.transactions = data.clothesItems;
+      console.log(data);
+      vm.transactions = data.transactions;
       for (var i = 0; i < vm.transactions.length; i++) {
-        if (vm.transactions[i].owner !== CurrentUserService.getUser().id){
+        if (vm.transactions[i].responder !== CurrentUserService.getUser().id){
           vm.transactions.splice(i,1);
           i--;
         }
