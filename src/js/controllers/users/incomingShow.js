281fc6d8ade8657dac384edcc682ed7c83bfe152
delamorinierejh <delamorinierejh@gmail.com>
2016-10-04 15:26:37 +0100

    ITS ALIVE

--- src/js/controllers/users/incomingShow.js
+++ src/js/controllers/users/incomingShow.js
@@ -9,9 +9,20 @@ angular
     .query({ responder : CurrentUserService.getUser().id})
     .$promise
     .then(data => {
-      console.log(data);
       vm.transactions = data.transactions;
+      for (var i = 0; i < vm.transactions.length; i++) {
+        if (!vm.transactions[i].initial_item.available) {
+          vm.transactions.splice(i,1);
+          i--;
+        } else if (vm.transactions[i].response_item){
+          if (vm.transactions[i].response_item.available === false) {
+            vm.transactions.splice(i,1);
+            i--;
+          }
+        }
+      }
     });
+
     vm.reject = (transaction) => {
       Transaction
       .reject({ _id: transaction._id })
--- src/js/controllers/users/outgoingShow.js
+++ src/js/controllers/users/outgoingShow.js
@@ -10,6 +10,17 @@ function OutgoingShowCtrl(Transaction, CurrentUserService, $stateParams, $state)
   .$promise
   .then(data => {
     vm.transactions = data.transactions;
+    for (var i = 0; i < vm.transactions.length; i++) {
+      if (!vm.transactions[i].initial_item.available) {
+        vm.transactions.splice(i,1);
+        i--;
+      } else if (vm.transactions[i].response_item){
+        if (vm.transactions[i].response_item.available === false) {
+          vm.transactions.splice(i,1);
+          i--;
+        }
+      }
+    }
   });
 
   vm.approve = (idhere) => {
