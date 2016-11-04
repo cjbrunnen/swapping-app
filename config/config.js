34d3a0e05c35e969dd2ac8078cf73d47a4d6fbf0
cjbrunnen <cazgotsoul@gmail.com>
2016-11-04 12:57:16 +0000

    adding final dependencies for hosting on Heroku

--- config/config.js
+++ config/config.js
@@ -1,8 +1,6 @@
 module.exports = {
   db: {
-    test : "mongodb://localhost/swish-list-test",
-    development : "mongodb://localhost/swish-list-development",
-    production : "mongodb://localhost/swish-list"
-  },
-  secret: process.env.SECRET || "gosh this is so secret... shhh..."
-};
+     test : "mongodb://localhost/swish-list-test",
+     development : "mongodb://localhost/swish-list-development",
+     production : process.env.MONGODB_URI || "mongodb://heroku_2jvlxxd2:ktog6l1pfsqqmqo8r2md10iqlv@ds143767.mlab.com:43767/heroku_2jvlxxd2"
+   },
